f4c1f831d3939ecb36690d53c5913bd99a67e494
KosmoKeLi <jerry@winterspring.se>
2020-04-30 08:00:01 +0200

    Fixed statusbar height on phones with top notch.

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -896,7 +896,7 @@ BOOL isExiting = FALSE;
         [self.toolbar setItems:@[self.closeButton, flexibleSpaceButton, self.backButton, fixedSpaceButton, self.forwardButton]];
     }
     
-    self.view.backgroundColor = [UIColor grayColor];
+    self.view.backgroundColor = [UIColor blackColor];
     [self.view addSubview:self.toolbar];
     [self.view addSubview:self.addressLabel];
     [self.view addSubview:self.spinner];
@@ -1097,13 +1097,37 @@ BOOL isExiting = FALSE;
     [self.webView goForward];
 }
 
+- (BOOL)hasTopNotch {
+    if (@available(iOS 11.0, *)) {
+        return [[[UIApplication sharedApplication] delegate] window].safeAreaInsets.top > 20.0;
+    }
+
+    return  NO;
+}
+
 - (void)viewWillAppear:(BOOL)animated
 {
     if (IsAtLeastiOSVersion(@"7.0") && !viewRenderedAtLeastOnce) {
         viewRenderedAtLeastOnce = TRUE;
         CGRect viewBounds = [self.webView bounds];
-        viewBounds.origin.y = STATUSBAR_HEIGHT;
-        viewBounds.size.height = viewBounds.size.height - STATUSBAR_HEIGHT;
+        
+        if ([self hasTopNotch]) {
+            BOOL toolbarVisible = !self.toolbar.hidden;
+            BOOL toolbarIsAtBottom = ![_browserOptions.toolbarposition isEqualToString:kInAppBrowserToolbarBarPositionTop];
+
+            float topSafeArea = [[[UIApplication sharedApplication] delegate] window].safeAreaInsets.top;
+            float bottomSafeArea = [[[UIApplication sharedApplication] delegate] window].safeAreaInsets.bottom;
+
+            if (toolbarVisible && toolbarIsAtBottom) {
+                bottomSafeArea = 0.0;
+            }
+
+            viewBounds.origin.y = topSafeArea;
+            viewBounds.size.height = viewBounds.size.height - (topSafeArea + bottomSafeArea);
+        } else {
+            viewBounds.origin.y = STATUSBAR_HEIGHT;
+            viewBounds.size.height = viewBounds.size.height - STATUSBAR_HEIGHT;
+        }
         self.webView.frame = viewBounds;
         [[UIApplication sharedApplication] setStatusBarStyle:[self preferredStatusBarStyle]];
     }
